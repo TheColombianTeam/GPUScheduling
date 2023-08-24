65538932be3e2697fb6c6ecd5469555aa99a93b9
Robert Limas <robalex123@outlook.es>
2023-08-24 08:08:00 -0500

    include workers apr to args

--- fi_campaign.py
+++ fi_campaign.py
@@ -3,7 +3,9 @@ from FaultInjector import fault_list
 from FaultInjector import injector
 from FaultInjector import validator
 
+from utils.args import args
+
 #golden(120,120,120)
 #fault_list()
-#injector(5)
+#injector(args.workers)
 validator(120,120)#this function needs output tensor dimentions inposed in golden for heat map bins 
